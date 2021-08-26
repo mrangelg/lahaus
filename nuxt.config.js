3add7735d20ff9f46f0910064a4019d0f3a2003d
Madelin Rangel <mrangel@utp.edu.co>
2021-08-25 20:30:38 -0500

    Axios installation and setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'lahouse',
+    title: 'lahaus',
     htmlAttrs: {
       lang: 'en'
     },
@@ -38,7 +38,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
+  modules: ['@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
