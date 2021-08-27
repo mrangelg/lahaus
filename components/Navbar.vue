afbc3b8561ac997a5f75ef0f3cae8ab7594bf791
Madelin Rangel <mrangel@utp.edu.co>
2021-08-26 21:02:26 -0500

    Incorporation of notification bubble on the navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,8 @@
 <template>
   <nav class="h-16 border-b border-gray-200 bg-white fixed w-full top-0 z-30">
-    <div class="flex items-center h-full md:hidden">
+    <div class="flex items-center h-full relative md:hidden">
       <img src="icons/bars.svg" alt="La Haus" class="pl-4" />
+      <NotificationBubble class="absolute top-3 left-10" />
       <NuxtLink to="/" class="pl-3 5">
         <img src="logo.svg" alt="La Haus" />
       </NuxtLink>
@@ -30,13 +31,14 @@
           <NuxtLink to="/" class="flex-1"> Recursor </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/" class="flex-1">
+          <NuxtLink to="/" class="flex-1 relative">
             Mi Perfil
             <img
               src="/icons/arrow-down.svg"
               alt="La Haus"
               class="inline-block"
             />
+            <NotificationBubble class="absolute top-0.5 -right-1.5" />
           </NuxtLink>
         </li>
       </ul>
---
+++ components/NotificationBubble.vue
@@ -0,0 +1,4 @@
+<template>
+  <span class="w-3 h-3 rounded-full bg-green-caribbean inline-block"></span>
+</template>
+
